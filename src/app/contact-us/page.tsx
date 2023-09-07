cc30fc8f839d5832a11c6a4bd012fc05f553a8e1
DinGo42 <ivan.nesterenko@ukr.net>
2023-09-07 16:09:35 +0300

    fixed issue with incorect import

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,4 +1,4 @@
-import { FeedBack, Map, PaymentMethod } from '@neon/widgets';
+import { Feedback, Map, PaymentMethod } from '@neon/widgets';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -8,7 +8,7 @@ export const metadata: Metadata = {
 const ContactUs = () => (
   <div className="flex justify-between w-full h-fit max-tabletS:flex-col">
     <div className="flex flex-col tabletS:h-full justify-between max-tabletS:gap-20 max-tabletM:w-full w-1/2">
-      <FeedBack />
+      <Feedback />
       <PaymentMethod />
     </div>
     <div className="max-tabletS:w-full w-full h-[650px] rounded-3xl border-4 border-green-700">
