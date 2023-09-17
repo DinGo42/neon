edd66b2b8a3d55d0d5c7d2338ff6a0af8498dde4
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-17 19:26:09 +0300

    ...

--- src/widgets/feedback/payment-method/payment-method.tsx
+++ src/widgets/feedback/payment-method/payment-method.tsx
@@ -2,16 +2,18 @@ import CashIcon from 'public/svgs/info/cash.svg';
 import CardIcon from 'public/svgs/info/card.svg';
 import TaxIcon from 'public/svgs/info/tax.svg';
 
-export const PaymentMethod = () => (
-  <div className="w-full h-min p-2 rounded-sm  pb-4 border-blue-700  flex flex-col gap-6">
-    <div className="flex items-center w-full gap-4">
-      <CashIcon /> <h2>[Готівка]</h2>
+export const PaymentMethod = () => {
+  return (
+    <div className="w-full h-min p-2 rounded-sm  pb-4 border-blue-700  flex flex-col gap-6">
+      <div className="flex items-center w-full gap-4">
+        <CashIcon /> <h2>[Готівка]</h2>
+      </div>
+      <div className="flex items-center w-full gap-4">
+        <CardIcon /> <h2>[Картка]</h2>
+      </div>
+      <div className="flex items-center w-full gap-4">
+        <TaxIcon /> <h2>[Карта + податки]</h2>
+      </div>
     </div>
-    <div className="flex items-center w-full gap-4">
-      <CardIcon /> <h2>[Картка]</h2>
-    </div>
-    <div className="flex items-center w-full gap-4">
-      <TaxIcon /> <h2>[Карта + налоги]</h2>
-    </div>
-  </div>
-);
+  );
+};
