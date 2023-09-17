c10c7e6559a04bd2a195fe2b70a7ca01f968352c
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-17 19:22:48 +0300

    removed PaymentMethod component

--- src/app/contact-us/page.tsx
+++ src/app/contact-us/page.tsx
@@ -1,4 +1,4 @@
-import { Feedback, Map, PaymentMethod } from '@neon/widgets';
+import { Feedback, Map } from '@neon/widgets';
 import { Metadata } from 'next';
 
 export const metadata: Metadata = {
@@ -6,21 +6,13 @@ export const metadata: Metadata = {
 };
 
 const ContactUs = () => (
-  <div className="flex justify-between w-full h-fit tabletS:items-center">
-    <div className="flex w-full max-tabletS:flex-col ">
-      <div className="flex flex-col tabletS:h-full justify-between max-tabletS:gap-10  gap-32 max-tabletM:w-full w-1/2">
-        <div className="flex flex-col gap-5">
-          <span className="text-3xl pl-2 text-pink-700">Спосіб оплати</span>
-          <Feedback />
-        </div>
-        <div className="flex flex-col gap-5">
-          <span className="text-3xl pl-2 text-pink-700">Спосіб оплати</span>
-          <PaymentMethod />
-        </div>
-      </div>
-      <div className="max-tabletS:w-full w-full max-tabletS:h-[450px] h-[600px]  rounded-3xl border-4 border-black-600">
-        <Map />
-      </div>
+  <div className="flex w-full max-tabletS:flex-col h-full justify-between">
+    <div className="flex flex-col h-fit justify-between max-tabletM:w-full w-1/2 gap-3">
+      <span className="text-3xl pl-2 text-pink-700">Зворотній зв`язок</span>
+      <Feedback />
+    </div>
+    <div className="max-tabletS:w-full w-full max-tabletS:h-[450px] h-[600px]  rounded-3xl border-4 border-black-600">
+      <Map />
     </div>
   </div>
 );
