826987c768585f521adf9afe569ce92ebe051d91
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-17 19:23:47 +0300

    changed paddings

--- src/app/services/[serviceType]/page.tsx
+++ src/app/services/[serviceType]/page.tsx
@@ -14,7 +14,7 @@ const Service: FC<ServiceProps> = ({ params: { serviceType } }) => {
   const { bgImg, serviceName, text } = service;
 
   return (
-    <div className="flex w-full h-full max-h-fit text-white gap-10 relative tabletS:p-20 ">
+    <div className="flex w-full h-full max-h-fit text-white gap-10 relative tabletM:p-10 tabletS:p-20">
       <Image
         priority
         src={bgImg}
@@ -23,23 +23,21 @@ const Service: FC<ServiceProps> = ({ params: { serviceType } }) => {
         alt={serviceName}
         className="opacity-20 z-0 inset-0 pointer-events-none rounded-2xl"
       />
-      <div className="flex mt-10  max-tabletM:flex-col">
-        <div className="flex flex-col p-5 gap-10 text-xl w-full ">
-          <span className="text-4xl text-orange-700 break-words">
+      <div className="flex max-tabletM:flex-col w-full">
+        <div className="flex flex-col p-3 gap-10 text-xl max-tabletM:w-full w-1/2">
+          <span className="text-4xl text-orange-700 break-words whitespace-pre-line">
             {serviceName}
           </span>
-          <pre className="whitespace-pre-wrap tabletM:pl-10 max-w-full">
-            {text}
-          </pre>
+          <pre className="whitespace-pre-line max-w-full">{text}</pre>
         </div>
-        <div className="tabletM:w-2/3 w-full h-96 relative">
+        <div className="tabletM:w-2/3 w-full tabletM:h-auto h-96 relative">
           <Image
             priority
             src={bgImg}
             layout="fill"
             objectFit="cover"
             alt={serviceName}
-            className="rounded-2xl opacity-60 "
+            className="rounded-2xl opacity-60"
           />
         </div>
       </div>
