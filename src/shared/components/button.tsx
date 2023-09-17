23f928a25d8b494804eb043502e950719ae3ced9
Ivan <91385595+DinGo42@users.noreply.github.com>
2023-09-17 19:24:28 +0300

    fixed issue with small carousel buttons

--- src/shared/components/button.tsx
+++ src/shared/components/button.tsx
@@ -4,7 +4,7 @@ import { twJoin } from 'tailwind-merge';
 
 export enum ButtonType {
   HEADER_BTN = 'bg-black-300 -skew-x-45 origin-center w-1/2 h-1/2 justify-center items-center p-2',
-  CARUSEL_BTN = 'h-full tabletM:h-fit tabletM:p-5 tabletM:bg-black-600 w-fit rounded-2xl max-tabletM:absolute opacity-20 tabletM:opacity-40 hover:border-blue-700 tabletM:border-2',
+  CARUSEL_BTN = 'h-full tabletM:h-fit tabletM:p-5 tabletM:bg-black-600 max-tabletM:w-1/2 w-fit rounded-2xl max-tabletM:absolute opacity-20 tabletM:opacity-40 hover:border-blue-700 tabletM:border-2',
   CHECK_BOX_BTN = 'p-4 h-fit w-fit rounded-2xl',
   NONE = '',
 }
